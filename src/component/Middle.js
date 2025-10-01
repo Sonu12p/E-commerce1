64191dcd3a3506757511569926ca79d0479fd097
Kartikesh Kumar <kartikesh.kumar@e-zest.in>
2025-10-01 11:51:59 +0530

    Login working

---
+++ src/component/Middle.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Carousel } from 'react-bootstrap';
+
+export const Middle = () => {
+  return (
+    <>
+    
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <Carousel>
+  
+    <div class="carousel-item active">
+        
+            <Carousel.Item>
+      <img src="gym.jpg" className="d-block w-100" alt="First slide" />
+      </Carousel.Item>
+    </div>
+    <div class="carousel-item">
+
+            <Carousel.Item>
+      <img src="gyms.jpg" className="d-block w-100" alt="..." />
+      </Carousel.Item>
+
+    </div>
+    <div class="carousel-item">
+            <Carousel.Item>
+      <img src="gym4.jpg" className="d-block w-100" alt="..." />
+      </Carousel.Item>
+    </div>  
+ 
+  </Carousel>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</>
+  )
+}
